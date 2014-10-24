8cca9b7be83b36828de2f1ec1206342674014f0e
Paul Coyle <paul.coyle@calgaryscientific.com>
2014-10-24 11:38:03 -0600

    Removing bump package.
    This is a bit of an opinionated change but it is nicer to prepare a
    release with the version bumped in a separate branch then tag it after
    being merged into master.  Otherwise we have a separate commit for the
    bump after the merge occurred even though the release was prepared.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,22 +14,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        bump: {
-            options: {
-                files: ['package.json'],
-                updateConfigs: ['pkg'],
-                commit: true,
-                commitMessage: 'Release %VERSION%',
-                commitFiles: ['package.json'],
-                createTag: true,
-                tagName: '%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: true,
-                pushTo: 'origin',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
-            }
-        },
-
         jsbeautifier: {
             options: {
                 js: {
@@ -94,7 +78,6 @@ module.exports = function(grunt) {
     grunt.loadTasks('tasks');
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-jsbeautifier');
